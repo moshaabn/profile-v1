c5dd10f58432fa34dc2399d5601f76a9cb50b1b7
Mo.Shaaban <mo.shaaban.dev@gmail.com>
2025-07-19 03:31:32 +0300

    feat: add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Delius_Swash_Caps, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const deliusSwashCaps = Delius_Swash_Caps({
@@ -82,6 +83,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0C2E3VF1X6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0C2E3VF1X6');
+          `}
+        </Script>
+      </head>
       <body
         className={`${deliusSwashCaps.variable} ${inter.variable} antialiased`}
       >
